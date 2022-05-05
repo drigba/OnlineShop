218fd0f79b70d04f8e8293dfe17fff9650367755
BVirag <79696174+BVirag@users.noreply.github.com>
2022-05-05 08:57:47 +0200

    imports

--- src/test/java/com/example/onlineshop/ProductServiceTest.java
+++ src/test/java/com/example/onlineshop/ProductServiceTest.java
@@ -13,8 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
