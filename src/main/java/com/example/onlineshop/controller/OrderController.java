428843c410a4a89a6ac265d9a49673c0525f93f2
Bánfi Zsombor <banfi.zsombor@aloha.hu>
2022-05-05 09:13:13 +0200

    Complete OrderController, RetailerController

--- src/main/java/com/example/onlineshop/controller/OrderController.java
+++ src/main/java/com/example/onlineshop/controller/OrderController.java
@@ -1,13 +1,64 @@
 package com.example.onlineshop.controller;
 
+import com.example.onlineshop.dtos.OrderDTO;
+
+import com.example.onlineshop.dtos.ProductDTO;
+import com.example.onlineshop.enums.OrderStatus;
+import com.example.onlineshop.enums.ProductType;
 import com.example.onlineshop.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/order")
 public class OrderController {
     @Autowired
     private OrderService orderService;
+
+    @PostMapping(path="/save")
+    public OrderDTO createOrder(@RequestBody OrderDTO orderDTO){
+        return orderService.createOrder(orderDTO);
+    }
+
+    @GetMapping(path="/getall")
+    public List<OrderDTO> getAllOrders(){
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping(path="/update")
+    public OrderDTO updateOrder(@RequestBody OrderDTO orderDTO, @RequestParam Integer id)
+    {
+        return orderService.updateOrder(orderDTO, id);
+    }
+
+    @PostMapping(path="/delete")
+    public ResponseEntity<Void> deleteOrder(@RequestParam Integer id)
+    {
+        try{
+            orderService.deleteOrder(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        catch(Exception e)
+        {
+            return new ResponseEntity<>(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+
+    @GetMapping(path="/getbystatus")
+    public List<OrderDTO> getOrdersByStatus(@RequestParam OrderStatus orderStatus)
+    {
+        return orderService.getOrdersByStatus(orderStatus);
+    }
+
+    @GetMapping(path="/getbyaddress")
+    public List<OrderDTO> getOrdersByAddress(@RequestParam String address)
+    {
+        return orderService.getOrdersByAddress(address);
+    }
+
+
 }
--- src/main/java/com/example/onlineshop/service/OrderService.java
+++ src/main/java/com/example/onlineshop/service/OrderService.java
@@ -1,11 +1,16 @@
 package com.example.onlineshop.service;
 
 import com.example.onlineshop.dtos.OrderDTO;
+import com.example.onlineshop.dtos.ProductDTO;
 import com.example.onlineshop.entity.Order;
+import com.example.onlineshop.enums.OrderStatus;
 import com.example.onlineshop.mapper.OrderMapper;
 import com.example.onlineshop.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
@@ -21,5 +26,58 @@ public class OrderService {
 
     private OrderDTO orderToDTO(Order order){
         return orderMapper.entityToDTO(order);
+    }
+
+
+    public OrderDTO createOrder(OrderDTO orderDTO){
+        Order order = dtoToOrder(orderDTO);
+        orderRepository.save(order);
+        return orderToDTO(order);
+    }
+
+    public OrderDTO updateOrder(OrderDTO orderDTO, Integer id) {
+        orderRepository.getById(id).setFromAddress(orderDTO.getFromAddress());
+        orderRepository.getById(id).setPrice(orderDTO.getPrice());
+        orderRepository.getById(id).setAddress(orderDTO.getAddress());
+        orderRepository.getById(id).setProductList(orderDTO.getProductList());
+        orderRepository.getById(id).setOrderStatus(orderDTO.getOrderStatus());
+        return orderToDTO(orderRepository.getById(id));
+    }
+
+    public void deleteOrder(Integer id) {
+        orderRepository.deleteById(id);
+    }
+
+
+
+
+    public List<OrderDTO> getAllOrders() {
+        return orderRepository.findAll()
+                .stream()
+                .map(this::orderToDTO)
+                .collect(Collectors.toList());
+    }
+
+
+
+    public List<OrderDTO> getSomeOrders(List<Order> sold) {
+        return sold
+                .stream()
+                .map(this::orderToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<OrderDTO> getOrdersByStatus(OrderStatus orderStatus) {
+        return orderRepository.findByOrderStatus(orderStatus)
+                .stream()
+                .map(this::orderToDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<OrderDTO> getOrdersByAddress(String address) {
+        return orderRepository.findByAddress(address)
+                .stream()
+                .map(this::orderToDTO)
+                .collect(Collectors.toList());
     }
 }
